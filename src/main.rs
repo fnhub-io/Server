22de69102f4e88b412f41c3a497506e0e4fd6471
kvvarun-mecha <varunkkv.mec@gmail.com>
2025-04-03 11:23:43 +0530

    feat: integrated minio

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,17 @@ mod actors;
 mod routes;
 mod wasmFunction;
 
+use actix::prelude::*;
+use actix::prelude::*;
 use actix_cors::Cors;
-use actix_web::{web, App, HttpServer, middleware};
-use actix::prelude::*;
+use actix_web::{get, post, HttpResponse, Responder};
+use actix_web::{middleware, web, App, HttpServer};
 use actors::WasmEngineActor;
-use actix::prelude::*;
-use actix_web::{get, post, HttpResponse,  Responder};
+use minio::s3::args::{BucketExistsArgs, MakeBucketArgs};
+use minio::s3::client::ClientBuilder;
+use minio::s3::creds::StaticProvider;
+use minio::s3::http::BaseUrl;
+use minio::s3::response::MakeBucketResponse;
 use routes::{execute_fn, test, upload_fn};
 
 #[actix_web::main]
@@ -15,11 +20,35 @@ async fn main() -> std::io::Result<()> {
     // Start WasmEngineActor
     let wasm_actor = WasmEngineActor {}.start();
 
+    //minio
+    let db_server = "http://localhost:9000".parse::<BaseUrl>().unwrap();
+    dbg!("Trying to connect to MinIO at: `{:?}`", &db_server);
+
+    let static_provider = StaticProvider::new("minioadmin", "minioadmin", None);
+
+    let client = ClientBuilder::new(db_server.clone())
+        .provider(Some(Box::new(static_provider)))
+        .ignore_cert_check(Some(true))
+        .build()
+        .unwrap();
+
+    let bucket_name = "neelabucket";
+    let args = BucketExistsArgs::new(bucket_name);
+
+    let resp = client.bucket_exists(&args.unwrap()).await.unwrap();
+
+    if !resp {
+        let mkbucket = MakeBucketArgs::new(bucket_name).unwrap();
+        client.make_bucket(&mkbucket).await.unwrap();
+    };
+
+    let client_data = web::Data::new(client);
+
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(wasm_actor.clone()))
-            .wrap(Cors::permissive())  // Enables CORS for all requests
-            .wrap(middleware::Logger::default()) // Logging Middleware
+            .wrap(Cors::permissive())
+            .wrap(middleware::Logger::default())
             .service(test)
             .service(execute_fn)
             .service(upload_fn)
--- src/routes.rs
+++ src/routes.rs
@@ -3,7 +3,13 @@ use actix::Addr;
 use actix_multipart::Multipart;
 use actix_web::{get, post, web, HttpResponse, Responder};
 use futures::{StreamExt, TryStreamExt};
-use std::path::Path;
+
+use minio::s3::{
+    args::{DownloadObjectArgs, GetObjectArgs, PutObjectArgs},
+    client::Client,
+};
+
+use std::{fs::File, path::Path};
 
 #[derive(serde::Deserialize)]
 struct ExecutePayload {
@@ -24,16 +30,33 @@ async fn test() -> impl Responder {
 async fn execute_fn(
     web::Json(payload): web::Json<ExecutePayload>,
     wasm_actor: web::Data<Addr<WasmEngineActor>>,
+    client: web::Data<Client>,
 ) -> impl Responder {
-    let addr = format!("./src/savedWasmFunctions/{}", payload.fn_name);
-    let path = Path::new(&addr);
-    
-    dbg!(payload.fn_name.clone(), path);
-    
+    let bucket_name = "Neelabucket";
+    let object_name = &payload.fn_name;
+    let download_path = format!("./src/savedWasmFunctions/{}", payload.fn_name);
+    let path = Path::new(&download_path);
+
+    // Download the file from MinIO
+
+    // let args = GetObjectArgs::new(bucket_name, object_name).unwrap();
+    // let get_object = client.get_object(&args).await;
+    let args = DownloadObjectArgs::new(bucket_name, object_name, download_path.as_str()).unwrap();
+    client.download_object(&args).await.unwrap();
+    // match get_object {
+    //     Ok(object) => {
+    //         object.content.to_file(path).await.unwrap();
+    //     }
+    //     Err(e) => {
+    //         return HttpResponse::InternalServerError()
+    //             .body(format!("Failed to download file: {}", e));
+    //     }
+    // }
+
     if path.exists() {
         let output = wasm_actor
             .send(ExecuteFn {
-                name: payload.fn_name,
+                name: payload.fn_name.clone(),
                 params: payload.params,
             })
             .await
@@ -48,7 +71,7 @@ async fn execute_fn(
 }
 
 #[post("/upload")]
-async fn upload_fn(mut payload: Multipart) -> impl Responder {
+async fn upload_fn(mut payload: Multipart, client: web::Data<Client>) -> impl Responder {
     // Process multipart form data
     dbg!("Upload being called");
     let mut fn_name = String::new();
@@ -84,7 +107,7 @@ async fn upload_fn(mut payload: Multipart) -> impl Responder {
         return HttpResponse::BadRequest().body("Missing function name or empty WASM file");
     }
 
-    // Save the file
+    // Save the file temporarily
     let path = Path::new("./src/savedWasmFunctions").join(&fn_name);
 
     // Create directory if it doesn't exist
@@ -97,7 +120,132 @@ async fn upload_fn(mut payload: Multipart) -> impl Responder {
 
     // Write the file
     match std::fs::write(&path, &wasm_data) {
-        Ok(_) => HttpResponse::Ok().body(format!("Function '{}' uploaded successfully", fn_name)),
+        Ok(_) => {
+            // Upload the file to MinIO
+            let bucket_name = "Neelabucket";
+            let object_name = &fn_name;
+            // let content = ObjectContent::from(path.clone());
+
+            let meta = std::fs::metadata(&path).unwrap();
+            let object_size = Some(meta.len() as usize);
+            let mut file = File::open(&path).unwrap();
+            let mut args =
+                PutObjectArgs::new(bucket_name, object_name, &mut file, object_size, None).unwrap();
+
+            match client.put_object(&mut args).await {
+                Ok(_) => {
+                    // Delete the temporary file
+                    std::fs::remove_file(&path).unwrap();
+                    HttpResponse::Ok().body(format!("Function '{}' uploaded successfully", fn_name))
+                }
+                Err(e) => HttpResponse::InternalServerError()
+                    .body(format!("Failed to upload file to MinIO: {}", e)),
+            }
+        }
         Err(e) => HttpResponse::InternalServerError().body(format!("Failed to write file: {}", e)),
     }
 }
+
+// use crate::actors::{ExecuteFn, WasmEngineActor};
+// use actix::Addr;
+// use actix_multipart::Multipart;
+// use actix_web::{get, post, web, HttpResponse, Responder};
+// use futures::{StreamExt, TryStreamExt};
+// use std::path::Path;
+
+// #[derive(serde::Deserialize)]
+// struct ExecutePayload {
+//     fn_name: String,
+//     params: Vec<String>,
+// }
+
+// #[get("/")]
+// async fn test() -> impl Responder {
+//     let port = std::env::var("PORT")
+//         .unwrap_or("8080".to_string())
+//         .parse::<u16>()
+//         .unwrap();
+//     HttpResponse::Ok().body(format!("The server is alive and running at port {}!", port))
+// }
+
+// #[post("/execute")]
+// async fn execute_fn(
+//     web::Json(payload): web::Json<ExecutePayload>,
+//     wasm_actor: web::Data<Addr<WasmEngineActor>>,
+// ) -> impl Responder {
+//     let addr = format!("./src/savedWasmFunctions/{}", payload.fn_name);
+//     let path = Path::new(&addr);
+
+//     dbg!(payload.fn_name.clone(), path);
+
+//     if path.exists() {
+//         let output = wasm_actor
+//             .send(ExecuteFn {
+//                 name: payload.fn_name,
+//                 params: payload.params,
+//             })
+//             .await
+//             .unwrap();
+//         match output {
+//             Ok(content) => HttpResponse::Ok().body(content),
+//             Err(e) => HttpResponse::InternalServerError().body(e.to_string()),
+//         }
+//     } else {
+//         HttpResponse::NotFound().body("Function not found")
+//     }
+// }
+
+// #[post("/upload")]
+// async fn upload_fn(mut payload: Multipart) -> impl Responder {
+//     // Process multipart form data
+//     dbg!("Upload being called");
+//     let mut fn_name = String::new();
+//     let mut wasm_data = Vec::new();
+
+//     while let Ok(Some(mut field)) = payload.try_next().await {
+//         let content_disposition = field.content_disposition();
+//         let field_name = content_disposition
+//             .as_ref()
+//             .and_then(|cd| cd.get_name())
+//             .unwrap_or("");
+
+//         if field_name == "fn_name" {
+//             // Process function name field
+//             while let Some(chunk) = field.next().await {
+//                 if let Ok(data) = chunk.map(|b| b.to_vec()) {
+//                     if let Ok(s) = std::str::from_utf8(&data) {
+//                         fn_name = s.to_string();
+//                     }
+//                 }
+//             }
+//         } else if field_name == "wasm_file" {
+//             // Process file field
+//             while let Some(chunk) = field.next().await {
+//                 if let Ok(data) = chunk {
+//                     wasm_data.extend_from_slice(&data);
+//                 }
+//             }
+//         }
+//     }
+
+//     if fn_name.is_empty() || wasm_data.is_empty() {
+//         return HttpResponse::BadRequest().body("Missing function name or empty WASM file");
+//     }
+
+//     // Save the file
+//     let path = Path::new("./src/savedWasmFunctions").join(&fn_name);
+
+//     // Create directory if it doesn't exist
+//     if let Some(parent) = path.parent() {
+//         if let Err(e) = std::fs::create_dir_all(parent) {
+//             return HttpResponse::InternalServerError()
+//                 .body(format!("Failed to create directory: {}", e));
+//         }
+//     }
+
+//     // Write the file
+//     match std::fs::write(&path, &wasm_data) {
+//         Ok(_) => HttpResponse::Ok().body(format!("Function '{}' uploaded successfully", fn_name)),
+//         Err(e) => HttpResponse::InternalServerError().body(format!("Failed to write file: {}", e)),
+//     }
+// }
